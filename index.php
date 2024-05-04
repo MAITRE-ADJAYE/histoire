94e721a120c9df710aaaaceb787b00fe8ca942f3
MAITRE-ADJAYE <160837809+MAITRE-ADJAYE@users.noreply.github.com>
2024-05-04 17:14:18 +0100

    mettre à jour histoire

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+  <html lang="fr">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Histoire</title>
+      <link rel="stylesheet" href="histoire.css">
+      <style>
+        
+      </style>
+    </head>
+    <body class="body">
+
+      <div class="fade-in">
+        <h1>Histoite</h1>
+      </div>
+
+      <div class="contenair-paragraphe">
+        <div class="fade-in-sous-titre1">
+          <h4>Origine du peuple Bantou</h4>
+        </div>
+          <div class="contenair"> 
+            <div class="scale-in">
+              <a href="img/carte_Gabon 2.png"><img class="image" src="img/carte_Gabon 1.png" alt="image"></a>
+            </div>
+            <p class="paragraphe slide">Les différentes sources que nous avons consultées nous expliquent que les Bakanigui font partie
+              des peuples BANTOU.Les peuples BANTOU, d’après les sources était originaire des hauts
+              plateaux du sud de l’Afrique de l’Ouest dans les actuels Nigéria et Cameroun.
+            </p>
+          </div>
+      </div>
+
+
+      <div class="contenair-paragraphe">
+        <div class="fade-in-sous-titre">
+          <h4>Migration Bantou vers le centre de l'Afrique</h4>
+        </div>
+        <div class="contenair"> 
+              <p class="paragraphe slide">Les différentes sources que nous avons consultées nous expliquent que les Bakanigui font partie
+                des peuples BANTOU.Les peuples BANTOU, d’après les sources était originaire des hauts
+                plateaux du sud de l’Afrique de l’Ouest dans les actuels Nigéria et Cameroun.
+              </p>
+
+            <div class="scale-in">
+              <a href="img/carte_Afrique 2.png"><img class="image" src="img/carte_Afrique 1.png" alt="image"></a>
+            </div>
+
+            <div class="scale-in_grand-lac">
+              <a href="img/bakanigui 4.png"><img class="image" src="img/carte_fontiere 1.png" alt="image"></a>
+            </div>
+
+            <div class="scale-in_plateaux-oubangui">
+              <a href="img/bakanigui 4.png"><img class="image" src="img/bakanigui 3.png" alt="image"></a>
+            </div>
+        </div>
+      </div>
+
+      
+      <div class="contenair-paragraphe">
+        <div class="fade-in-sous-titre">
+          <h4>Localisation initiale du peuple Bakanigui</h4>
+        </div>
+          <div class="contenair"> 
+            <div class="scale-in">
+            <a href="img/bakanigui 2.png"><img class="image" src="img/bakanigui 1.png" alt="image"></a>
+            </div>
+            <p class="paragraphe slide">
+            Le peuple Bakanigui se trouvait parmi ces peuples bantous installés en République du Congo et principalement dans la cuvette ouest, 
+            dans le district d’Ewo à une quarantaine de mètre de la frontière du GABON.
+          </p>
+          </div>
+      </div>
+
+
+
+      <div class="contenair-paragraphe">
+        <div class="fade-in-sous-titre">
+          <h4>Migration du Peuple Bakanigui vers le GABON</h4>
+        </div>
+          <div class="contenair"> 
+          <p class="paragraphe_1 slide-in">
+        Les MBOCHIS, vinrent s'installer dans la région d’Ewo et entreprit des guerres contre tous les peuples de la région. 
+        Les Bakanigui décidèrent donc de quitter la région d’Ewo. 
+        Ils remontèrent par la Sébé (Affluent) de l’ogooué et s’installèrent au GABON dans un premier temps dans la zone d’Okondja. 
+        Ensuite, les Obamba repoussés par les MBOCHIS vinrent s’installer également dans la zone d’Okondja. 
+        Leur premier village était Andjokou, sur une montagne à côté d’Okondja. Ils trouvèrent sur la Sébé le chef Oshashi, 
+        qui commandait aux Akanigui, venus les premiers avec les Pygmées (Akoula ou Abongo). 
+        Ils luttèrent contre les Bakanigui et les Pygmées et les repoussèrent sur l’Ogooué. 
+        Les Bakanigui vinrent alors s’installer dans la région de la basse Passa où on les retrouve jusqu’à aujourd’hui.
+      </p>
+
+            <div class="scale-in">
+          <a href="img/carte_Afrique 2.png"><img class="image" src="img/carte_Afrique 1.png" alt="image"></a>
+          </div>
+
+      </div>
+
+
+
+    </body>
+  </html>
